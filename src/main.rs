a126904153a421cd351e74e058f4b7d73639d229
Adam Wilson <wilsonadam66@gmail.com>
2024-06-24 21:42:26 -0400

    added tailwind

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use yew::prelude::*;
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <h1>{"Hello, world!"}</h1>
+        <h1 class={classes!("bg-red-300")}>{"Hello, world!"}</h1>
     }
 }
 
--- target/wasm-bindgen/debug/ladder_league.js
+++ target/wasm-bindgen/debug/ladder_league.js
@@ -630,12 +630,12 @@ function __wbg_get_imports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper853 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper975 = function(arg0, arg1, arg2) {
         const ret = makeClosure(arg0, arg1, 126, __wbg_adapter_20);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3185 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 330, __wbg_adapter_23);
+    imports.wbg.__wbindgen_closure_wrapper3508 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 338, __wbg_adapter_23);
         return addHeapObject(ret);
     };
 
